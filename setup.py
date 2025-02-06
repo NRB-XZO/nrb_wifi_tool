7be7da2e217494d6fa6e2c79a43248418e816ebb
NRB-XZO <78638685+NRB-XZO@users.noreply.github.com>
2025-02-06 10:42:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 import os
 import time
+from os import system
+system("python3 -m venv venv")
+system("source ven/bin/activate")
+system("pip install -r requirements.txt")
 os.system("apt-get install pip")
 print("pip installation complete")
 time.sleep(1)
 os.system("pip install subprocess.run")
 os.system("pip install instaloader")
 os.system("pip install pytest-shutil")
-os.system("pip install pyclamd)
+os.system("pip install pyclamd")
 os.system("pip install beautifulsoup4")
 os.system("pip install bs4")
 print("package installations completed")
